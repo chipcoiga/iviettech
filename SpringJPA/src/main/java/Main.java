9522cf3bdc256537e6238fb29344c6e337013b32
chipcoiga <lehuuloc.qt@gmail.com>
2023-07-08 09:20:57 +0700

    create jpa module

---
+++ SpringJPA/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import config.BeanConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+
+    }
+}
