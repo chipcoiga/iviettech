3e3601946c8ae19e0c72beaa9e68f967729dd436
chipcoiga <lehuuloc.qt@gmail.com>
2023-11-09 19:25:52 +0700

    revert annotation config

--- HuuHoc_hellowork/src/main/java/annotationconfiguration/Main.java
+++ HuuHoc_hellowork/src/main/java/annotationconfiguration/Main.java
@@ -1,5 +1,7 @@
 package annotationconfiguration;
 
+import annotationconfiguration.pojobeans.AccountService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
 
 import java.security.InvalidKeyException;
@@ -9,6 +11,19 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 //        List.of().stream().filter(i -> i > 5).collect(Collectors.toList());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("annotationbasedconfiguration/beans.xml");
+        AccountService accountService = context.getBean("accountService", AccountService.class);
+
+        System.out.println("Before money transfer ");
+        System.out.println("Account 1 balance : " + accountService.getAccount(1).getBalance());
+        System.out.println("Account 2 balance : " + accountService.getAccount(2).getBalance());
+
+        accountService.transferMoney(1,2,5.0);
+        System.out.println("==============================");
+        System.out.println("after money transfer ");
+        System.out.println("Account 1 balance : " + accountService.getAccount(1).getBalance());
+        System.out.println("Account 2 balance : " + accountService.getAccount(2).getBalance());
     }
 }
 
