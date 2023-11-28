10636e455c3a46bf2f5bc426e429d3950359285a
chipcoiga <lehuuloc.qt@gmail.com>
2023-11-28 18:08:36 +0700

    Demo add to cart

---
+++ ShopingCartJava51/src/main/java/com/example/shopingcartjava51/ShopingCartJava51Application.java
@@ -0,0 +1,13 @@
+package com.example.shopingcartjava51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingCartJava51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopingCartJava51Application.class, args);
+    }
+
+}
---
+++ ShopingCartJava51/src/test/java/com/example/shopingcartjava51/ShopingCartJava51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopingcartjava51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingCartJava51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
