9ecdb9af1cdeef394f8cf382ef3337e7aa49f051
chipcoiga <lehuuloc.qt@gmail.com>
2024-04-06 18:14:07 +0700

    demo date time

--- demoForQuan/src/main/java/quandaica/com/demoforquan/domain/UpSertProduct.java
+++ demoForQuan/src/main/java/quandaica/com/demoforquan/domain/UpSertProduct.java
@@ -2,10 +2,42 @@ package quandaica.com.demoforquan.domain;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class UpSertProduct {
     private String name;
     private Double price;
     private MultipartFile file;
+
+    private LocalDateTime createdDateTime;
+    private LocalDate createdDate;
+    private LocalTime createdTime;
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalTime createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
 
     public String getName() {
         return name;
